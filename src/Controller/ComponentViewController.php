5b07e3970d01d128817d2f43746d7c2a4b90e47a
Alexander Gerber <alex.gerber90@gmail.com>
2018-12-15 14:28:19 +0100

    fix frontend style for component view

--- src/Controller/ComponentViewController.php
+++ src/Controller/ComponentViewController.php
@@ -7,9 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ComponentRepository;
 use App\Entity\Component;
+use App\Repository\CategoryRepository;
+use App\Repository\TagRepository;
+use App\Interfaces\JsonDatafields;
+use App\Traits\JsonArrayToArrayClasses;
 
-class ComponentViewController extends AbstractController
+class ComponentViewController extends AbstractController implements JsonDatafields
 {
+
+    use JsonArrayToArrayClasses;
+
     /**
      * @var Component
      */
@@ -20,11 +27,23 @@ class ComponentViewController extends AbstractController
      */
     private $componentRepository;
 
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
+    
+    /**
+     * @var TagRepository
+     */
+    private $tagRepository;
 
     public function __construct(
-        ComponentRepository $componentRepository
+        ComponentRepository $componentRepository, CategoryRepository $categoryRepository,
+        TagRepository $tagRepository
     ) {
         $this->componentRepository = $componentRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->tagRepository = $tagRepository;
     }
 
     /**
@@ -36,8 +55,21 @@ class ComponentViewController extends AbstractController
             'link' => $slug 
         ]);
 
+        $categories = $this->getArrayClasses($this->component->getCategories(), $this->categoryRepository);
+        $tags = $this->getArrayClasses($this->component->getTags(), $this->tagRepository);
+
+        $component = [
+            'id' => $this->component->getId(),
+            'link' => $this->component->getLink(),
+            'title' => $this->component->getTitle(),
+            'description' => $this->component->getDescription(),
+            'picture' => $this->component->getPicture(),
+            'categories' => $categories,
+            'tags' => $tags,
+        ];
+
         return $this->render('05-pages/component-view.html.twig',[
-            'component' => $this->component
+            'component' => $component
         ]);
     
     }
