d7a998cee6b1ff15f183614be7dd1a84ad69de0f
Alexander Gerber <alex.gerber90@gmail.com>
2018-12-20 23:14:48 +0100

    fix entity wron repo name

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\TagsRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
  */
 class Tag
 {
