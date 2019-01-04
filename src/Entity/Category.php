1bea5cf88b9e5ae8332dc0b02ce995aa714a4c2f
Alexander Gerber <alex.gerber90@gmail.com>
2019-01-04 17:29:50 +0100

    remove anused entrys

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  */
+
 class Category
 {
     /**
@@ -27,28 +28,6 @@ class Category
      * @ORM\Column(type="string", length=255)
      */
     private $component_link;
-
-    /**
-     * @var Project[]|ArrayCollection
-     *
-     * @ORM\ManyToMany(targetEntity="App\Entity\Project")
-     * @ORM\OrderBy({"name": "ASC"})
-     */
-    private $projects;
-
-    /**
-     * @var Component[]|ArrayCollection
-     *
-     * @ORM\ManyToMany(targetEntity="App\Entity\Component")
-     * @ORM\OrderBy({"name": "ASC"})
-     */
-    private $components;
-
-    public function __construct()
-    {
-        $this->projects = new ArrayCollection();
-        $this->components = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -77,43 +56,5 @@ class Category
         $this->component_link = $component_link;
 
         return $this;
-    }
-
-    public function addProject(?Project ...$Projects): void
-    {
-        foreach ($Projects as $Project) {
-            if (!$this->projects->contains($Project)) {
-                $this->projects->add($Project);
-            }
-        }
-    }
-
-    public function removeProject(Project $Project): void
-    {
-        $this->projects->removeElement($Project);
-    }
-
-    public function getProjects(): Collection
-    {
-        return $this->projects;
-    }
-
-    public function addComponent(?Component ...$components): void
-    {
-        foreach ($components as $component) {
-            if (!$this->components->contains($component)) {
-                $this->components->add($component);
-            }
-        }
-    }
-
-    public function removeComponent(Component $component): void
-    {
-        $this->components->removeElement($component);
-    }
-
-    public function getComponents(): Collection
-    {
-        return $this->components;
     }
 }
--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -29,29 +29,6 @@ class Tag
      */
     private $component_link;
 
-    /**
-     * @var Project[]|ArrayCollection
-     *
-     * @ORM\ManyToMany(targetEntity="App\Entity\Project")
-     * @ORM\OrderBy({"name": "ASC"})
-     */
-    private $projects;
-
-    /**
-     * @var Component[]|ArrayCollection
-     *
-     * @ORM\ManyToMany(targetEntity="App\Entity\Component")
-     * @ORM\OrderBy({"name": "ASC"})
-     */
-    private $components;
-
-
-    public function __construct()
-    {
-        $this->projects = new ArrayCollection();
-        $this->components = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -79,43 +56,5 @@ class Tag
         $this->component_link = $component_link;
 
         return $this;
-    }
-
-    public function addProject(?Project ...$Projects): void
-    {
-        foreach ($Projects as $Project) {
-            if (!$this->projects->contains($Project)) {
-                $this->projects->add($Project);
-            }
-        }
-    }
-
-    public function removeProject(Project $Project): void
-    {
-        $this->projects->removeElement($Project);
-    }
-
-    public function getProjects(): Collection
-    {
-        return $this->projects;
-    }
-
-    public function addComponent(?Component ...$components): void
-    {
-        foreach ($components as $component) {
-            if (!$this->components->contains($component)) {
-                $this->components->add($component);
-            }
-        }
-    }
-
-    public function removeComponent(Component $component): void
-    {
-        $this->components->removeElement($component);
-    }
-
-    public function getComponents(): Collection
-    {
-        return $this->components;
     }
 }
