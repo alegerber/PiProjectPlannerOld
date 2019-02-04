20525d0df4679389da507b9068d6e7c9a741631c
Alexander Gerber <alex.gerber90@gmail.com>
2019-02-04 20:24:42 +0100

    add slug test

---
+++ tests/Utils/SluggerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Utils;
+
+use App\Utils\Slugger;
+use PHPUnit\Framework\TestCase;
+
+class SluggerTest extends TestCase
+{
+    public function testSlugify()
+    {
+        $result = Slugger::slugify('sfaERq awsw sda');
+
+        $this->assertEquals('sfaerq-awsw-sda', $result);
+    }
+}
