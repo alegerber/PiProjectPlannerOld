b6dd01b4b972113209cc51c8790f701f09276313
Alexander Gerber <alex.gerber90@gmail.com>
2019-02-25 22:38:06 +0100

    improve test for uploaded formhandling

--- tests/Services/FormHandlingTest.php
+++ tests/Services/FormHandlingTest.php
@@ -7,24 +7,39 @@ use App\Kernel;
 use App\Entity\Image;
 use App\Services\FormHandling;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class FormHandlingTest extends TestCase
 {
-    /**
-     *
-     */
-    public function testHandle(): void
+    public function testUploadedFileHandle(): void
+    {
+        $container = $this->getContainer();
+        $formHandling = $container->get(FormHandling::class);
+
+        $image = $this->setImage($container);
+
+        $oldScanDir = \scandir('/var/www/html/public/uploads/images', SCANDIR_SORT_NONE);
+        $formHandling->uploadedFileHandle($image, $container->getParameter('image_file_directory'));
+
+        $newScanDir = \scandir('/var/www/html/public/uploads/images', SCANDIR_SORT_NONE);
+        \unlink('/var/www/html/public/uploads/images/' . \implode(\array_diff($newScanDir, $oldScanDir)));
+
+        $this->assertTrue(\count($oldScanDir) + 1 === \count($newScanDir));
+    }
+
+    private function getContainer(): ContainerInterface
     {
         $kernel = new Kernel(
             $_SERVER['APP_ENV'] ?? 'dev',
             $_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))
         );
         $kernel->boot();
-        $container = $kernel->getContainer();
 
-        $formHandling = $container->get(FormHandling::class);
+        return $kernel->getContainer();
+    }
+    private function setImage(ContainerInterface $container): Image
+    {
         $entityManager = $container->get('doctrine.orm.default_entity_manager');
 
         $entrys = 100;
@@ -49,15 +64,6 @@ class FormHandlingTest extends TestCase
             $tagAll[\random_int(0, $length)],
             $tagAll[\random_int(0, $length)]
         );
-
-        $oldScanDir = \scandir('/var/www/html/public/uploads/images', SCANDIR_SORT_NONE);
-
-        $formHandling->uploadedFileHandle($image, $container->getParameter('image_file_directory'));
-
-        $newScanDir = \scandir('/var/www/html/public/uploads/images', SCANDIR_SORT_NONE);
-
-        \unlink('/var/www/html/public/uploads/images/' . \implode(\array_diff($newScanDir, $oldScanDir)));
-
-        $this->assertTrue(\count($oldScanDir) + 1 === \count($newScanDir));
+        return $image;
     }
 }
