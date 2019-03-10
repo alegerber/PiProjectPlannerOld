ccd98e459751b18412644bafa0bc81b0f9f3c6de
Alexander Gerber <alex.gerber90@gmail.com>
2019-03-10 16:04:21 +0100

    add test for dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,10 +10,17 @@ class DashboardController extends AbstractController
 {
     /**
      * @Route("/", methods={"GET"}, name="dashboard")
+     * @param \Twig_Environment $twig
      * @return Response
      */
-    public function index(): Response
+    public function index(\Twig_Environment $twig): Response
     {
-        return $this->render('05-pages/dashboard.html.twig');
+        try {
+            return new Response(
+                $twig->render('05-pages/dashboard.html.twig')
+            );
+        } catch (\Twig_Error $e) {
+            return new Response($e->getMessage(), 500);
+        }
     }
 }
---
+++ tests/Services/DashboardControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Tests\Services;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use App\Controller\DashboardController;
+
+class DashboardControllerTest extends TestCase
+{
+    public function testIndex(): void
+    {
+        /** @var MockObject $categoryController */
+        $twig = $this->getMockBuilder(\Twig_Environment::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $twig->expects($this->once())
+            ->method('render')
+            ->with('05-pages/dashboard.html.twig');
+
+        /** @var DashboardController $dashboardController */
+        $dashboardController = new DashboardController();
+
+        $dashboardController->index($twig);
+    }
+}
