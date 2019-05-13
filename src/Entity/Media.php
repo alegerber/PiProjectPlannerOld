e9ca7f72a2eba9be80601315cc140e38995fcb03
Alexander Gerber <alex.gerber90@gmail.com>
2019-05-14 00:08:49 +0200

    fix

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
